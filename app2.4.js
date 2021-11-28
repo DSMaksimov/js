00ce4a82878e5cb0646a5c07df48b1015405a7aa
DSMaksimov <89150458+DSMaksimov@users.noreply.github.com>
2021-11-28 16:27:39 +0300

    Домашнее задание №2

---
+++ app2.4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let a = 4;
+let b = 9;
+
+function f1(a, b) {
+    return a + b;
+};
+
+function f2(a, b) {
+    return a - b;
+};
+
+function f3(a, b) {
+    return a * b;
+};
+function f4(a, b) {
+    return a / b;
+};
+
